7a8ccdc33c03599b5f8eb72bb3930790a4b339c3
Izabelle wilding <izabellewilding@gmail.com>
2023-12-31 16:59:03 +0000

    filter by magnitude

--- src/app/components/Charts.tsx
+++ src/app/components/Charts.tsx
@@ -4,9 +4,9 @@ import { ErrorBoundary } from "./ErrorBoundary";
 function Chart() {
   const { data, error, isLoading } = useEarthquakeQuery();
 
-  console.log(
-    data && data.features.map((feature: any) => console.log(feature))
-  );
+  const earthquakesFilteredByMagnitude = data?.features
+    .map((feature: any) => feature)
+    .filter((item: any) => item.properties.mag > 6.0);
 
   if (isLoading) {
     return <div>Loading...</div>;
