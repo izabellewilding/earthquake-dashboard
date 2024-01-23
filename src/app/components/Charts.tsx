f5e9a7c824781df04751db31b6b7a3186f348ed4
Izabelle wilding <izabellewilding@gmail.com>
2024-01-23 16:59:55 +0000

    Move queries into charts component

--- src/app/components/Charts.tsx
+++ src/app/components/Charts.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   CategoryScale,
   LinearScale,
@@ -14,6 +16,14 @@ import { BarChart } from "./BarChart";
 import { Card } from "./Card";
 import { List } from "./List";
 import { Earthquake, MonthData, CardData } from "../types/types";
+import axios from "axios";
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { groupByMonthMagnitude } from "../utils/groupByMonthMagnitude";
+import ClipLoader from "react-spinners/ClipLoader";
+import { groupBy } from "../utils/GroupChartData";
+import { getCurrentDateTime } from "../utils/getCurrentDataTime";
+import { getDateOneMonthAgo } from "../utils/getDateOneMonthAgo";
 
 ChartJS.register(
   ArcElement,
@@ -62,27 +72,100 @@ ChartJS.overrides.doughnut = {
   },
 };
 
-interface ChartsComponentProps {
-  earthquakesPerCountry: Object;
-  earthquakesByMonthMagnitude: MonthData[];
-  latestEarthquake: CardData;
-  largestEarthquakeInLastMonth: CardData;
-  last10Earthquakes: Earthquake[];
+//query setup
+
+async function queryEarthquakeAPI(query: string) {
+  const response = await axios.get(
+    `https://earthquake.usgs.gov/fdsnws/event/1/query${query}`
+  );
+
+  const data = response.data;
+
+  return data;
 }
 
-function ChartsComponent({
-  earthquakesPerCountry,
-  earthquakesByMonthMagnitude,
-  latestEarthquake,
-  largestEarthquakeInLastMonth,
-  last10Earthquakes,
-}: ChartsComponentProps) {
+interface ChartsComponentProps {}
+
+function ChartsComponent({}: any) {
+  const currentDate = getCurrentDateTime();
+  const dateOneMonthAgo = getDateOneMonthAgo();
+
+  const monthMagnitudeQuery =
+    "?format=geojson&starttime=2023-01-01&endtime=2023-12-31&minmagnitude=6.0";
+
+  const earthquakesByMonthMagnitude = useQuery({
+    queryKey: ["earthquakes by month and magnitude", monthMagnitudeQuery],
+    queryFn: async () => {
+      const data = await queryEarthquakeAPI(monthMagnitudeQuery);
+      return groupByMonthMagnitude(data);
+    },
+  });
+
+  const lastWeek = "?format=geojson&starttime=2024-01-07&endtime=2024-01-08";
+
+  const earthquakesPerCountry = useQuery({
+    queryKey: ["earthquakes per country", lastWeek],
+    queryFn: async () => {
+      const data = await queryEarthquakeAPI(lastWeek);
+
+      return groupBy(data.features, "place");
+    },
+  });
+
+  const earthquakesInLastMonthQuery = `?format=geojson&starttime=${dateOneMonthAgo}&endtime=${currentDate}`;
+
+  const allEarthquakesInLastMonth = useQuery({
+    queryKey: ["allEarthquakesInLastMonth", earthquakesInLastMonthQuery],
+    queryFn: async () => {
+      const data = await queryEarthquakeAPI(earthquakesInLastMonthQuery);
+      return data;
+    },
+  });
+
+  const largestEarthquakeInLastMonth =
+    allEarthquakesInLastMonth?.data?.features.reduce(
+      (a: Earthquake, b: Earthquake) =>
+        a.properties.mag > b.properties.mag ? a : b
+    );
+
+  const latestEarthquake = allEarthquakesInLastMonth?.data?.features[0];
+
+  const last10Earthquakes =
+    allEarthquakesInLastMonth?.data?.features.slice(-10);
+
+  const isLoading =
+    earthquakesByMonthMagnitude.isLoading ||
+    allEarthquakesInLastMonth.isLoading ||
+    earthquakesPerCountry.isLoading;
+
+  const error =
+    earthquakesByMonthMagnitude.error ||
+    allEarthquakesInLastMonth.error ||
+    earthquakesPerCountry.error;
+
+  if (isLoading) {
+    return (
+      <ClipLoader
+        color={"blue"}
+        loading={isLoading}
+        cssOverride={{}}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    );
+  }
+
+  if (error) {
+    return "An error occured, please try refreshing the page...";
+  }
+
   const barChartData = {
-    labels: earthquakesByMonthMagnitude.map((item: any) => item.label),
+    labels: earthquakesByMonthMagnitude.data.map((item: any) => item.label),
     datasets: [
       {
         label: "USGS Earthquakes",
-        data: earthquakesByMonthMagnitude.map((item: any) => item.value),
+        data: earthquakesByMonthMagnitude.data.map((item: any) => item.value),
         backgroundColor: [
           "#3498db", // Dodger Blue
           "#2ecc71", // Emerald Green
@@ -112,12 +195,12 @@ function ChartsComponent({
   };
 
   const doughnutChartData = {
-    labels: Object.keys(earthquakesPerCountry || {}),
+    labels: Object.keys(earthquakesPerCountry.data || {}),
     datasets: [
       {
         label: "USGS Earthquakes",
-        data: Object.values(earthquakesPerCountry || {}).map(
-          (earthquakes) => earthquakes.length
+        data: Object.values(earthquakesPerCountry.data || {}).map(
+          (earthquakes: any) => earthquakes.length
         ),
         backgroundColor: [
           "#3498db", // Dodger Blue
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,111 +1,12 @@
-"use client";
-
 import React from "react";
-import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
 import Charts from "./components/Charts";
 import { SideNavbar } from "./components/SideNavbar";
-import { groupByMonthMagnitude } from "./utils/groupByMonthMagnitude";
-import ClipLoader from "react-spinners/ClipLoader";
-import { groupBy } from "./utils/GroupChartData";
-import { getCurrentDateTime } from "./utils/getCurrentDataTime";
-import { getDateOneMonthAgo } from "./utils/getDateOneMonthAgo";
-import { Earthquake } from "./types/types";
-
-async function queryEarthquakeAPI(query: string) {
-  const response = await axios.get(
-    `https://earthquake.usgs.gov/fdsnws/event/1/query${query}`
-  );
-
-  const data = response.data;
-
-  return data;
-}
 
 export default function Home() {
-  const currentDate = getCurrentDateTime();
-  const dateOneMonthAgo = getDateOneMonthAgo();
-
-  const monthMagnitudeQuery =
-    "?format=geojson&starttime=2023-01-01&endtime=2023-12-31&minmagnitude=6.0";
-
-  const earthquakesByMonthMagnitude = useQuery({
-    queryKey: ["earthquakes by month and magnitude", monthMagnitudeQuery],
-    queryFn: async () => {
-      const data = await queryEarthquakeAPI(monthMagnitudeQuery);
-      return groupByMonthMagnitude(data);
-    },
-  });
-
-  const lastWeek = "?format=geojson&starttime=2024-01-07&endtime=2024-01-08";
-
-  const earthquakesPerCountry = useQuery({
-    queryKey: ["earthquakes per country", lastWeek],
-    queryFn: async () => {
-      const data = await queryEarthquakeAPI(lastWeek);
-
-      return groupBy(data.features, "place");
-    },
-  });
-
-  const earthquakesInLastMonthQuery = `?format=geojson&starttime=${dateOneMonthAgo}&endtime=${currentDate}`;
-
-  const allEarthquakesInLastMonth = useQuery({
-    queryKey: ["allEarthquakesInLastMonth", earthquakesInLastMonthQuery],
-    queryFn: async () => {
-      const data = await queryEarthquakeAPI(earthquakesInLastMonthQuery);
-      return data;
-    },
-  });
-
-  const largestEarthquakeInLastMonth =
-    allEarthquakesInLastMonth?.data?.features.reduce(
-      (a: Earthquake, b: Earthquake) =>
-        a.properties.mag > b.properties.mag ? a : b
-    );
-
-  const latestEarthquake = allEarthquakesInLastMonth?.data?.features[0];
-
-  const last10Earthquakes =
-    allEarthquakesInLastMonth?.data?.features.slice(-10);
-
-  const isLoading =
-    earthquakesByMonthMagnitude.isLoading ||
-    allEarthquakesInLastMonth.isLoading ||
-    earthquakesPerCountry.isLoading;
-
-  const error =
-    earthquakesByMonthMagnitude.error ||
-    allEarthquakesInLastMonth.error ||
-    earthquakesPerCountry.error;
-
-  if (isLoading) {
-    return (
-      <ClipLoader
-        color={"blue"}
-        loading={isLoading}
-        cssOverride={{}}
-        size={150}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
-    );
-  }
-
-  if (error) {
-    return "An error occured, please try refreshing the page...";
-  }
-
   return (
     <main>
       <SideNavbar>
-        <Charts
-          earthquakesPerCountry={earthquakesPerCountry.data}
-          earthquakesByMonthMagnitude={earthquakesByMonthMagnitude.data || {}}
-          latestEarthquake={latestEarthquake}
-          largestEarthquakeInLastMonth={largestEarthquakeInLastMonth}
-          last10Earthquakes={last10Earthquakes}
-        />
+        <Charts />
       </SideNavbar>
     </main>
   );
