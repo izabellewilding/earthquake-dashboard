01c5c4d687693f1b86dd6ca25512862efd0521af
Izabelle wilding <izabellewilding@gmail.com>
2024-10-08 14:51:42 +0100

    Remove side navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { SideNavbar } from "./components/SideNavbar";
 
 export default function Home() {
   return (
-    <main className="bg-background">
-      <SideNavbar>
-        <Charts />
-      </SideNavbar>
+    <main className="bg-slate-800">
+      {/* <SideNavbar> */}
+      <Charts />
+      {/* </SideNavbar> */}
     </main>
   );
 }
