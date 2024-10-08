c70c12cc06b01a7ff225075d42862ea50e7a993e
Izabelle wilding <izabellewilding@gmail.com>
2024-10-08 14:52:08 +0100

    Edit Recent Activity List styling

--- src/app/components/List.tsx
+++ src/app/components/List.tsx
@@ -6,13 +6,13 @@ interface ListProps {
 
 export const List = ({ data }: ListProps) => {
   return (
-    <div className="max-h-fit p-8 md:m-6 sm:ml-8  rounded-md bg-gradient-to-b ">
-      <h2 className="text-lg font-bold pb-6 text-text">Recent Activity</h2>
+    <div className="max-h-fit p-6 md:m-5 sm:ml-8 rounded-md bg-gradient-to-b bg-slate-800 shadow-2xl">
+      <h2 className="text-md font-bold pb-6 text-text">Recent Activity</h2>
       <ul>
         {data?.map((earthquake: any, index: number) => (
           <li
             key={`${index}${earthquake.properties.id}`}
-            className="pb-3 text-slate-400"
+            className="pb-3 text-sm text-slate-400 hover:text-slate-200 cursor-pointer transition-all"
           >
             <p>{`(M${earthquake.properties.mag}) ${earthquake.properties.place}`}</p>
           </li>
